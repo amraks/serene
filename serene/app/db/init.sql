8f44a4db87285b173c08e35e5aad20674c534b5c
dtang <dtang@egnyte.com>
2017-07-09 14:02:50 -0700

    Add docker startup script for postgres

---
+++ serene/app/db/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS serene.public.user (
+    name VARCHAR(50),
+    email VARCHAR(50),
+    password_hash CHAR(128) NOT NULL,
+    salt CHAR(32) NOT NULL,
+    creation_date DATE,
+    last_login TIMESTAMP,
+    PRIMARY KEY (email)
+);
