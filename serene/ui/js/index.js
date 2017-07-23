86d5cc43ba27c140cd03a0547deaf5f94727426c
David Tang <dtangster@yahoo.com>
2017-07-23 19:26:16 +0000

    Add bootstrap "Hi" button with react component

--- serene/ui/js/index.js
+++ serene/ui/js/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 const App = function() {
-    return <div>Hi</div>;
+    return <div><button className="btn btn-default">Hi</button></div>;
 }
 
 ReactDOM.render(<App />, document.querySelector(".app"));
