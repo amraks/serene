2719ad5725b24d777b473360f2f79f2f5f6be3b6
David Tang <dtangster@yahoo.com>
2017-07-30 19:13:06 +0000

    App placeholders for components, containers, reducers, and actions

---
+++ serene/ui/src/js/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div><button className="btn btn-default">Hi</button></div>
+        );
+    }
+}
--- serene/ui/src/js/index.js
+++ serene/ui/src/js/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap/dist/css/bootstrap.css';
-
-const App = function() {
-    return <div><button className="btn btn-default">Hi</button></div>;
-}
+import { App } from './components/app';
 
 ReactDOM.render(<App />, document.querySelector(".app"));
