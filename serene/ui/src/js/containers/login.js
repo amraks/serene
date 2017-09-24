7c7cb345b841633c45111ccf82a65d9c31cea8b9
David Tang <david.tang@yandex.com>
2017-09-24 22:37:24 +0000

    Modal state via redux

--- serene/ui/src/js/containers/login.js
+++ serene/ui/src/js/containers/login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Modal, Button, Popover, Navbar, Tooltip, OverlayTrigger, Form, FormGroup, FormControl, Col} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { unloadLoginModal, loadSignUpModal, unloadSignUpModal } from '../actions/modalPopUpActions';
 
 export class Login extends React.Component {
 
@@ -19,7 +21,7 @@ export class Login extends React.Component {
   render() {
     console.log('Login modal render');
     return (
-      <Modal show={this.props.showLoginCallback()} onHide={this.props.setLoginModalStateCallback}>
+      <Modal show={this.props.showLogin} onHide={this.props.unloadLoginModal}>
         <Modal.Header closeButton>
           <Modal.Title>Log into your account!</Modal.Title>
         </Modal.Header>
@@ -43,11 +45,23 @@ export class Login extends React.Component {
               </FormGroup>
             </Form>
           </Navbar.Form>
-          <Link to="/signup">
-            Don't have an account? Sign up
-          </Link>
+          <div>
+            Don't have an account?<Button type="button" onClick="this.props.loadSignUpModal">Sign Up</Button>
+          </div>
         </Modal.Body>
       </Modal> 
     ) 
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    showLogin : state.ui.showLoginModal
+  }
+}
+
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({ unloadLoginModal : unloadLoginModal, loadSignUpModal : loadSignUpModal} , dispatch)
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(Login);
