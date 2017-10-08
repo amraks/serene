6ab84ceb817e6b5cb798fc719b504d9e289768c8
Harsh Savla <hsavla@egnyte.com>
2017-10-08 16:25:48 -0700

    Sign up

--- serene/ui/src/js/actions/userActions.js
+++ serene/ui/src/js/actions/userActions.js
@@ -1,8 +1,12 @@
 const LOGIN_URL = '/login'
+const SIGN_UP_URL = '/signup'
 
 export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL';
 export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
 export const USER_LOGOUT = 'USER_LOGOUT';
+export const USER_SIGN_UP = 'USER_SIGN_UP';
+export const USER_SIGNUP_SUCCESS = 'USER_SIGNUP_SUCCESS';
+export const USER_SIGNUP_FAIL = 'USER_SIGNUP_FAIL';
 
 let userLogInSuccess = email => {
   console.log('In userLogInSuccess, email=', email)
@@ -15,6 +19,20 @@ let userLogInSuccess = email => {
 let userLoginFail = () => {
   return {
     type: USER_LOGIN_FAIL,
+    payload: null
+  }
+}
+
+let userSignUpSuccess = email => {
+  return {
+    type: USER_SIGNUP_SUCCESS,
+    payload: email
+  }
+}
+
+let userSignUpFail = () => {
+  return {
+    type: USER_SIGNUP_FAIL,
     payload: null
   }
 }
@@ -48,6 +66,33 @@ export const doLogin = (email, password) => {
     })
     .catch(error => {
       dispatch(userLoginFail())
+    })
+  }
+}
+
+export const doSignUp = (email, passwd, vpasswd) => {
+  return dispatch => {
+    fetch(SIGN_UP_URL, {
+      method : 'post',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify({
+        email : email,
+        password : password,
+        verifyPassword : vpasswd
+      })
+    })
+    .then(response => {
+      console.log('response=', response)
+      return response.json()
+    })
+    .then(json => {
+      console.log('/signup json=', json)
+      dispatch(userSignUpSuccess(json['email']))
+    })
+    .catch(error => {
+      dispatch(userSignUpFail())
     })
   }
 }
