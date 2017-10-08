d5c1775556f3b0eb603a1c9f218fb3b291573db0
Harsh Savla <hsavla@egnyte.com>
2017-10-08 16:00:13 -0700

    fix fetch request to /login

--- serene/ui/src/js/actions/userActions.js
+++ serene/ui/src/js/actions/userActions.js
@@ -29,7 +29,14 @@ export const doLogout = () => {
 export const doLogin = (email, password) => {
   return dispatch => {
     fetch(LOGIN_URL, {
-      method : 'post'
+      method : 'post',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify({
+        email : email,
+        password : password
+      })
     })
     .then(response => {
       console.log('response=', response)
