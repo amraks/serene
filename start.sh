50a76068db1c2b0062bb21c962b9c6088a8da5ae
David Tang <david.tang@yandex.com>
2017-12-10 19:51:20 +0000

    Remove david as default project name

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ set -e
 git submodule update --init --recursive
 docker volume create --name coderunner
 docker volume create --name mongostore
-docker-compose -p david build --force-rm
-docker-compose -p david up
+docker-compose build --force-rm
+docker-compose up
